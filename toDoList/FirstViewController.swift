6596ccb14eccc0c70f541bc47fa96871662f05c1
yhazir2017 <yildiray.hazir@gmail.com>
2017-08-17 22:38:11 -0700

    Initial Commit

---
+++ toDoList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  toDoList
+//
+//  Created by YILDIRAY HAZIR on 8/17/17.
+//  Copyright © 2017 interview. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ toDoList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  toDoList
+//
+//  Created by YILDIRAY HAZIR on 8/17/17.
+//  Copyright © 2017 interview. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
