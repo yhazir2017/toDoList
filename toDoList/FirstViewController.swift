65dee30c6d74e9ed9d5eab66a5399535920bd5ed
yhazir2017 <yildiray.hazir@gmail.com>
2017-08-17 23:42:11 -0700

    ok

--- toDoList/FirstViewController.swift
+++ toDoList/FirstViewController.swift
@@ -8,8 +8,39 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = ["Go to Safeway","Buy Egg","Pay the Bill","Return to Cart", "Go to Home"]
 
+class FirstViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return (toDoList.count)
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = toDoList[indexPath.row]
+        cell.textLabel?.highlightedTextColor = UIColor.red
+        return(cell)
+    }
+    
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            toDoList.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- toDoList/SecondViewController.swift
+++ toDoList/SecondViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBAction func addButton(_ sender: Any) {
+        if (inputText.text != ""){
+            toDoList.append(inputText.text!)
+            inputText.text = ""
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
